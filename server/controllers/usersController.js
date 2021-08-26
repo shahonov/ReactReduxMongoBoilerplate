a2fb9a23763f6438203d66a7d6863b69b893b816
Martin Shahonov <shahonov@gmail.com>
2021-08-26 22:56:43 +0300

    added endpoints

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,60 @@
 const router = require('express').Router();
+const NodeRSA = require('node-rsa');
 
-router.get('/users', async (req, res) => {
-    res.send();
+const { getUser, setUser } = require('../data/usersData');
+const { readPrivateKey } = require('../inMemory/cryptoPairs');
+
+// session will expire one hour after sign in
+const generateExpirationDate = () => {
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+    return now.getTime();
+}
+
+router.post('/sign-in', async (req, res, next) => {
+    try {
+        const { email, encryptionId, encryptedPassword } = req.body;
+
+        const privateKey = readPrivateKey(encryptionId);
+        if (!privateKey) {
+            res.json({ isSuccess: false, message: 'could not get private rsa key' });
+        }
+        const rsa = new NodeRSA(privateKey);
+        const decryptedPassword = rsa.decrypt(encryptedPassword, 'utf8');
+
+        const result = await getUser(email, decryptedPassword);
+        const { password, ...rest } = result;
+        if (rest) {
+            res.json({ isSuccess: true, user: { expiration: generateExpirationDate(), ...rest } });
+        } else {
+            res.json({ isSuccess: false, message: 'invalid credentials' });
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/sign-up', async (req, res, next) => {
+    try {
+        const { email, encryptionId, encryptedPassword } = req.body;
+
+        const privateKey = readPrivateKey(encryptionId);
+        if (!privateKey) {
+            res.json({ isSuccess: false, message: 'could not get private rsa key' });
+        }
+        const rsa = new NodeRSA(privateKey);
+        const password = rsa.decrypt(encryptedPassword, 'utf8');
+
+        const result = await setUser(email, password);
+        // TODO: send account activation email
+        if (result) {
+            res.json({ isSuccess: true });
+        } else {
+            res.json({ isSuccess: false, message: 'could not create user' });
+        }
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
